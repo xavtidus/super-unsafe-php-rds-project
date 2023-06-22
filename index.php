d653de42a79cbca3574f8c771fb126e492e2108d
Xavier Hutchinson <xh7359@gmail.com>
2023-06-22 12:23:49 +1200

    updates

--- index.php
+++ index.php
@@ -8,26 +8,4 @@ echo "starting..."
 $json = file_get_contents('/tmp/config.json');
 
 echo $json
-
-// Decode JSON file into an associative array
-$config = json_decode($json, true);
-
-// Extract values from the config
-$servername = $config['servername'];
-$username = $config['username'];
-$password = $config['password'];
-$dbname = $config['dbname'];
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connected successfully";
-
-// Always remember to close the connection once you're done
-$conn->close();
 ?>
