e491d458a1e9faec5b9b9c7a71aaff35bf0899ed
Xavier Hutchinson <xh7359@gmail.com>
2023-06-22 12:19:35 +1200

    updates

--- index.php
+++ index.php
@@ -4,19 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // Read JSON file
 echo "starting..."
-$json = file_get_contents('config.json');
 
-$tmp_cfg_file_path = "/tmp/config.json"
-
-if (file_exists($tmp_cfg_file_path)) {
-    $message = "The config in the temp dir exists, loading";
-    $json = file_get_contents($tmp_cfg_file_path);
-} else {
-    $message = "The config in the temp dir does not exist, loading defaults";
-    $json = file_get_contents('config.json');
-}
-
-echo $message
+$json = file_get_contents('/tmp/config.json');
 
 // Decode JSON file into an associative array
 $config = json_decode($json, true);
