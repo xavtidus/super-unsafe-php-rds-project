b6780c503874b52d2fe9d95279d001058b446b93
Xavier Hutchinson <xh7359@gmail.com>
2023-06-22 12:24:43 +1200

    updates

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 // Read JSON file
 echo "starting..."
 
 $json = file_get_contents('/tmp/config.json');
-
-echo $json
 ?>
