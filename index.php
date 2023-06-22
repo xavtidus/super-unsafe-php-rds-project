39b89c3174c221db9e4163cebfe90d7f52f3141c
Xavier Hutchinson <xh7359@gmail.com>
2023-06-22 12:26:46 +1200

    updates

--- index.php
+++ index.php
@@ -4,7 +4,38 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Read JSON file
-echo "starting..."
+echo "starting...";
 
-$json = file_get_contents('/tmp/config.json');
+$config_file_path = "/tmp/config.json";
+
+if(file_exists()){
+    $json = file_get_contents($config_file_path);
+
+    echo $json;
+
+    // Decode JSON file into an associative array
+    $config = json_decode($json, true);
+
+    // Extract values from the config
+    $servername = $config['servername'];
+    $username = $config['username'];
+    $password = $config['password'];
+    $dbname = $config['dbname'];
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    echo "Connected successfully";
+
+    // Always remember to close the connection once you're done
+    $conn->close();
+}
+else{
+    echo "config does not exist in: $config_file_path";
+}
 ?>
