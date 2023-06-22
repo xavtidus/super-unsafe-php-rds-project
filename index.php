433f1323d7e474aad14c779b33642ddba22692fe
Xavier Hutchinson <xh7359@gmail.com>
2023-06-22 12:34:02 +1200

    updates

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if(file_exists($config_file_path)){
         die("Connection failed: " . $conn->connect_error);
     }
 
-    echo "\r\n\r\nConnected successfully";
+    echo "<br /><br /><br />Connected successfully";
 
     // Always remember to close the connection once you're done
     $conn->close();
